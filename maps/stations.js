2de3d9949fb27d89835e8cc9c75bdb32c22aeb79
Gilles De Mey <gilles.de.mey@gmail.com>
2014-08-12 00:17:27 -0700

    Initial stations mapping and time zone fixes

---
+++ maps/stations.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var _       = require('lodash-node'),
+    moment  = require('moment');
+
+exports.mapStation = function(data) {
+
+  var trains = _.map(data.Trains, function(train) {
+
+    return {
+      'arrival'         : moment(train.ArrivalDateTime).zone('+0200').unix() || null,
+      'arrival_str'     : (function() {
+        return train.ArrivalDateTime === null
+          ? null
+          : moment(train.ArrivalDateTime).zone('+0200').fromNow();
+      })(),
+      'arrival_delay'   : train.ArrivalDelay,
+      'has_arrived'     : train.ArrivalDetected,
+
+      'departure'       : moment(train.DepartureDateTime).zone('+0200').unix() || null,
+      'departure_str'   : (function() {
+        return train.DepartureDateTime === null
+          ? null
+          : moment(train.DepartureDateTime).zone('+0200').fromNow();
+      })(),
+      'departure_delay' : train.DepartureDelay,
+      'has_departed'    : train.DepartureDetected,
+
+      'track'           : train.Track,
+
+      'origin'          : train.Origins[0],
+      'destination'     : train.Destinations[0],
+
+      'type'            : train.CommercialTypes[0] || null,
+      'train_number'    : train.TrainNumber,
+
+      // modifications to regular schedule
+      'route_modified'  : train.RouteModified,
+      'track_modified'  : train.TrackModified,
+    };
+
+  });
+
+  // TODO: lookup station id in Redis DB to send additional info
+  return {
+    'id'      : data.Id,
+    'trains'  : trains
+  };
+
+};
--- routes/stations.js
+++ routes/stations.js
@@ -1,14 +1,24 @@
+'use strict';
+
 var express   = require('express'),
     request   = require('request'),
     router    = express.Router(),
     moment    = require('moment'),
+    _         = require('lodash-node'),
+    maps      = require('../maps/stations.js'),
     OAuth     = require('../lib/oauth');
 
 /* GET status */
-router.get('/:id([0-9]+)', function(req, res, next) {
+router.get('/:id([0-9]+)/:subset(arrivals|departures)?', function(req, res, next) {
 
-  var dateTimeFrom = moment().zone('+01:00').format('YYYY-MM-DD HH:mm:ss');
-  var dateTimeTo   = moment().zone('+01:00').add(1, 'hours').format('YYYY-MM-DD HH:mm:ss');
+  var dateTimeFrom = moment()
+    .zone('+0200')
+    .format('YYYY-MM-DD HH:mm:ss');
+
+  var dateTimeTo   = moment()
+    .zone('+0200')
+    .add(1, 'hours')
+    .format('YYYY-MM-DD HH:mm:ss');
 
   var params = {
     'dateTimeFrom'  : dateTimeFrom,
@@ -17,10 +27,27 @@ router.get('/:id([0-9]+)', function(req, res, next) {
     'stationID'     : req.params.id
   };
 
-  request.get(OAuth('RetrieveStationSchedule', params), function(err, resp, body) {
+  request.get(new OAuth('RetrieveStationSchedule', params), function(err, resp, body) {
 
     if (err || resp.statusCode !== 200)
       return res.send(resp.statusCode || 500, { error: '¯\\_(ツ)_/¯' });
+
+    body = maps.mapStation(body);
+
+    var sort, reject;
+
+    if (req.params.subset === 'arrivals') {
+      sort = 'arrival';
+      reject = function(train) { return train.arrival === null; };
+    } else if (req.params.subset === 'departures') {
+      sort = 'departure';
+      reject = function(train) { return train.departure === null; };
+    }
+
+    body.trains = _.chain(body.trains)
+      .reject(reject)
+      .sortBy(sort)
+      .value();
 
     res.json(body);
   });
