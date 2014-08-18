498d4d728d0fa38cc63608315369d4ab08828793
Gilles De Mey <gilles.de.mey@gmail.com>
2014-08-17 20:55:19 -0700

    Added locale to disruptions and a language map

---
+++ maps/locale.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.translate = function(locale) {
+
+  switch(locale) {
+    case 'fr':
+      return 0;
+    case 'nl':
+      return 1;
+    case 'de':
+      return 2;
+    case 'en':
+      return 3;
+    default:
+      return 3;
+  }
+
+};
--- routes/disruptions.js
+++ routes/disruptions.js
@@ -4,7 +4,7 @@ var express   = require('express'),
     request   = require('request'),
     router    = express.Router(),
     // _         = require('lodash-node'),
-    // maps      = require('../maps/disruptions.js'),
+    locale    = require('../maps/locale.js'),
     OAuth     = require('../lib/oauth');
 
 /* GET status */
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 
   var params = {
     'detailLevel' : 'all',
-    'language'    : 3
+    'language'    : locale.translate(req.locale)
   };
 
   request.get(new OAuth('RetrieveInfoMessagesForSmartPhone', params), function(err, resp, body) {
