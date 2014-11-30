68b0d453c6aa384ac267c12bd857dabe4e0f819d
Michiel De Mey <de.mey.michiel@gmail.com>
2014-11-30 21:26:20 +0100

    Added utf-8 charset to the json response for correct parsing.

--- routes/all.js
+++ routes/all.js
@@ -7,6 +7,7 @@ var router = express.Router();
 router.get('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
   next();
 });
 
